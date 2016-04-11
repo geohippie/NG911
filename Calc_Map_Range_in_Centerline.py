329a40d1213744447f477e4edbec9350784ffabc
Lee <geohippie@users.noreply.github.com>
2016-04-11 14:49:48 -0500

    Update Calc_Map_Range_in_Centerline.py

--- Calc_Map_Range_in_Centerline.py
+++ Calc_Map_Range_in_Centerline.py
@@ -1,53 +1,45 @@
 #-------------------------------------------------------------------------------
-# Name:        module2
-# Purpose:
+# Name:        Calculate Field Map Range or Label expression with Map Range
+# Purpose:      Calculate Field Map Range or Label expression with Map Range
 #
-# Author:      allenlee
+# Author:      Lee Allen
 #
-# Created:     18/08/2015
-# Copyright:   (c) allenlee 2015
-# Licence:     <your licence>
+# Created:     04/11/2016
+# Copyright:   (c) Lee Allen 2016
+# License:     Use at your earliest convenience
 #-------------------------------------------------------------------------------
-# takes the "L_F_ADD","L_T_ADD","R_F_ADD","R_T_ADD" fields and adds them to a field name MAP_RANGE
-# MAP_RANGE is used for labeling centerlines to easily see ranges of all segments
-import arcpy
+#For just labeling---->Below
 
-fc = r'\\sncogis\Departments\GIS and Mapping\NGen911\Shawnee_KSNG911N08062015\Shawnee_KSNG911N_Final.gdb\NG911\RoadCenterline'
+#While in Editing Session - Right Click MAP RANGE field in table>Field Calculator
+#Enter data as shown below
 
-# Start an edit session. Must provide the worksapce.
-edit = arcpy.da.Editor(r'\\sncogis\Departments\GIS and Mapping\NGen911\Shawnee_KSNG911N08062015\Shawnee_KSNG911N_Final.gdb')
+#select Python
+#check Show Codeblock
 
-# Edit session is started without an undo/redo stack for versioned data
-#  (for second argument, use False for unversioned data)
-edit.startEditing(False, False)
+# Pre-Logic Script Code:
+def map_range(L_T_ADD,R_T_ADD,R_F_ADD,L_F_ADD):
+    lst = []
+    lst.append(L_T_ADD)
+    lst.append(R_T_ADD)
+    lst.append(R_F_ADD)
+    lst.append(L_F_ADD)
+    mrange = str(min(lst)) + ' - ' + str(max(lst))
+    return mrange
 
-# Start an edit operation
-edit.startOperation()
+#MAP_RANGE =
+map_range( !L_T_ADD!, !R_T_ADD!, !R_F_ADD!, !L_F_ADD!)
 
+##---------------LABEL ADDRESS RANGES-----------------------------
+##  USE INSTEAD of Calculating field
+##  Check Display coded value description
+##  Check Advanced
+##  Parser is Python
 
-fields = ["L_F_ADD","L_T_ADD","R_F_ADD","R_T_ADD","MAP_RANGE"]
-
-count = 0
-with arcpy.da.UpdateCursor(fc,fields) as cursor:
-    for row in cursor:
-        count = count + 1
-        lst = []
-        L_T_ADD = row[1]
-        R_T_ADD = row[3]
-        R_F_ADD = row[2]
-        L_F_ADD = row[0]
-        lst.append(L_T_ADD)
-        lst.append(R_T_ADD)
-        lst.append(R_F_ADD)
-        lst.append(L_F_ADD)
-        row[4] = str(min(lst)) + ' - ' + str(max(lst))
-        cursor.updateRow(row)
-        print "Row",count,"Updated" #count the rows as they are updated
-
-# Stop the edit operation.
-edit.stopOperation()
-
-# Stop the edit session and save the changes
-edit.stopEditing(True)
-
-Print "Update Done"
+def FindLabel ( [L_T_ADD] , [R_T_ADD] , [R_F_ADD] , [L_F_ADD]  ):
+    lst = []
+    lst.append([L_T_ADD])
+    lst.append([R_T_ADD])
+    lst.append([R_F_ADD])
+    lst.append([L_F_ADD])
+    mrange = str(min(lst)) + ' - ' + str(max(lst))
+    return mrange
